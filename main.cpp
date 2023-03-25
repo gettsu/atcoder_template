d775a2099b98aa221f9f72333654f5ab5eb98012
gettsu <tsugedaisuke.dev@gmail.com>
2023-03-25 17:37:49 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using P = pair<ll, ll>;
+using dP = pair<double, double>;
+using umap = unordered_map<ll, ll>;
+#define rep(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define rep2(i, s1, n) for (ll i = (s1); i < (ll)(n); i++)
+#define rrep(i, a, b) for (ll i = (a); i >= (ll)(b); i--)
+#define fastio                                                                 \
+    ios::sync_with_stdio(false);                                               \
+    cin.tie(0);                                                                \
+    cout.tie(0)
+#define all(x) (x).begin(), (x).end()
+#define uniq(x) (x).erase(unique(all(x)), (x).end())
+#define pb push_back
+#define endl "\n"
+#define precise fixed << setprecision(15)
+const ll inf = 1LL << 60;
+
+int main() {
+}
